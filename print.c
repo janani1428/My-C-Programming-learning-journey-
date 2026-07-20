709b9f6503e634c82e9fd3c3b3ff93281a20623d
janani1428 <jjanani319@gmail.com>
2026-07-20 19:04:05 +0530

    Day 1 - Introduction to C

---
+++ print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    printf("hello world!\n");
+    printf("STUDENT\n");
+    printf("mahendra collage of engineering\n");
+    printf("Electronics and communication engineering\n");
+    printf("Salem\n");
+    return 0;
+
+}
